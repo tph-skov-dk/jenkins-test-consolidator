2a849f9db5c3c89e80167f74281b8c0bbb261515
Theis Pieter Hollebeek <tph@skov.dk>
2026-02-12 14:52:15 +0100

    accept user input

--- main.ts
+++ main.ts
@@ -1,7 +1,25 @@
 import { parseJobs } from "./parsing.ts";
 import { render } from "./render.ts";
 import { buildTree } from "./tree.ts";
+import * as fs from "@std/fs";
+import * as path from "@std/path";
 
 if (import.meta.main) {
-    await render(buildTree(await parseJobs("test_input")), "out");
+    const target = Deno.args[0];
+    const out = Deno.args[1] ?? "out";
+    if (!target) {
+        console.warn("no target specified");
+        console.warn(`  hint: try <binary_path> <target> <output>`);
+        Deno.exit(1);
+    }
+    if (!await fs.exists(path.join(target, "jobs"))) {
+        console.warn(`expected '${target}' to have a jobs/ folder`);
+        console.warn(
+            `  hint: try specifying target as jenkins user working dir`,
+        );
+        Deno.exit(1);
+    }
+    await render(buildTree(await parseJobs(Deno.args[0])), out);
+    console.warn(`rendered to '${out}'`);
+    Deno.exit(0);
 }
